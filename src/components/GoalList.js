af7a0b5bca2d1129fd5fcfad4be51a96919487da
leomcg <leomcg@gmail.com>
2025-02-05 19:46:51 -0300

    creating first components

---
+++ src/components/GoalList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./GoalList.css";
+
+const GoalList = () => {
+  return (
+    <ul className="goal-list">
+      <li>Learn all about the course</li>
+      <li>Fisinish all tasks</li>
+      <li>Help other students</li>
+    </ul>
+  );
+};
+
+export default GoalList;
